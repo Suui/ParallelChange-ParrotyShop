f394f07e602f17a04e5f030e2a9540189bf8b16b
Suui <suui.gd@gmail.com>
2020-03-11 17:54:17 +0100

    Project init

---
+++ src/test/java/AmazingTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AmazingTest {
+
+    @Test
+    public void xxx() {
+        assertThat(true).isFalse();
+    }
+}
